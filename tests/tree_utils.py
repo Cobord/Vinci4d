a452b2ad2b045f81f0e2e2e260da01430c73497b
Cobord <ammar.s.husain@gmail.com>
2025-03-17 04:46:57 -0400

    - rename file with main generator - assertions on trees reconstructed from level sequences

--- tests/tree_utils.py
+++ tests/tree_utils.py
@@ -4,7 +4,7 @@ the methods on OrderedTree
 
 from typing import List
 from hypothesis import given, strategies as st
-from tree_enumeration.brute_recurse import OrderedTree
+from tree_enumeration import OrderedTree
 
 def test_simple_trees():
     """
@@ -22,13 +22,21 @@ def test_simple_trees():
     assert tree_test.num_leaves == 3
     assert str(tree_test) == "(()()())"
     assert tree_test.level_sequence() == [0,1,1,1]
-    assert OrderedTree.from_level_sequence([0,1,1,1]).level_sequence() == [0,1,1,1]
+    remade_tree_test = OrderedTree.from_level_sequence([0,1,1,1])
+    assert remade_tree_test.num_nodes == 4
+    assert remade_tree_test.num_leaves == 3
+    assert str(remade_tree_test) == "(()()())"
+    assert remade_tree_test.level_sequence() == [0,1,1,1]
     tree_test = OrderedTree([OrderedTree.degenerate(), tree_test], False)
     assert tree_test.num_nodes == 6
     assert tree_test.num_leaves == 4
     assert str(tree_test) == "(()(()()()))"
     assert tree_test.level_sequence() == [0,1,1,2,2,2]
-    assert OrderedTree.from_level_sequence([0,1,1,2,2,2]).level_sequence() == [0,1,1,2,2,2]
+    remade_tree_test = OrderedTree.from_level_sequence([0,1,1,2,2,2])
+    assert remade_tree_test.level_sequence() == [0,1,1,2,2,2]
+    assert remade_tree_test.num_nodes == 6
+    assert remade_tree_test.num_leaves == 4
+    assert str(remade_tree_test) == "(()(()()()))"
     cur_str = "()"
     for idx in range(2,10):
         tree_test = OrderedTree.degenerate_path(idx)
@@ -36,10 +44,12 @@ def test_simple_trees():
         assert tree_test.num_leaves == 1
         assert tree_test.level_sequence() == list(range(idx))
         assert str(tree_test) == f"({cur_str})"
+        remade_tree_test = OrderedTree.from_level_sequence(list(range(idx)))
+        assert remade_tree_test.level_sequence() == list(range(idx))
+        assert remade_tree_test.num_nodes == idx
+        assert remade_tree_test.num_leaves == 1
+        assert str(remade_tree_test) == f"({cur_str})"
         cur_str = f"({cur_str})"
-        assert OrderedTree\
-            .from_level_sequence(list(range(idx)))\
-            .level_sequence() == list(range(idx))
 
 @given(st.lists(st.integers(0,5),min_size=1,max_size=10))
 def round_trip(trial_level_sequence: List[int]):
--- tree_enumeration/__init__.py
+++ tree_enumeration/__init__.py
@@ -2,5 +2,5 @@
 relevant for external use 
 """
 from .beyer_hedetniemi import beyer_hedetniemi
-from .brute_recurse import tree_gen
+from .exhaustive_tree_gen import tree_gen
 from .ordered_tree import OrderedTree
